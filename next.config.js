4e73a6872579efe1872b26af0d6accb200e7e8f0
clearly-outsane <1starwarrior516@gmail.com>
2023-03-08 21:05:07 +0530

    initial push

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/api/:slug*",
+        destination: "https://api.wh.geniussports.com/v1/:slug*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
