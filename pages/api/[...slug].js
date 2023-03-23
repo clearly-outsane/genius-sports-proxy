90eb4b8df4748ca374e0a96d6063fb18377d26b9
clearly-outsane <1starwarrior516@gmail.com>
2023-03-23 23:50:53 +0530

    works?

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -32,6 +32,7 @@ export default (req, res) => {
   console.log(req.query);
 
   return new Promise((resolve, reject) => {
+    req.url = req.url.replace("/api", "/");
     proxy.web(
       req,
       res,
